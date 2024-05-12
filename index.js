49909cc6aa49484f5d2b9881dc97a603e3824089
edrftgyhu7 <js1gianis13@hotmail.com>
2024-05-12 14:31:51 +0700

    init

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-function lengthOfLIS(nums) {
-  if (nums.length === 0) return 0;
-  const dp = new Array(nums.length).fill(1);
-  let max = 1;
-  for (let i = 1; i < nums.length; i++) {
-    for (let j = 0; j < i; j++) {
-      if (nums[i] > nums[j]) dp[i] = Math.max(dp[i], dp[j] + 1);
-    }
-    max = Math.max(max, dp[i]);
+function groupAnagrams(strs) {
+  const map = new Map();
+  for (const str of strs) {
+    const sortedStr = str.split("").sort().join("");
+    if (!map.has(sortedStr)) map.set(sortedStr, []);
+    map.get(sortedStr).push(str);
   }
-  return max;
+  return [...map.values()];
 }
